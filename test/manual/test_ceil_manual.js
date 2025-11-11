7f90dda5e8b054455c4ac900ec7213444be4c9d6
Jyri Ketonen <jyri.ketonen@tuni.fi>
2025-11-11 20:09:00 +0200

    initial yaml

---
+++ test/manual/test_ceil_manual.js
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import ceil from "../../src/ceil.js";
+
+describe("Tests for ceil.js", () => {
+  it("ceil(2.1), should return 3", () => {
+    expect(ceil(2.1)).to.equal(3);
+  });
+  it("ceil(4.001, 2), should return 4.01", () => {
+    expect(ceil(4.001, 2)).to.equal(4.01);
+  });
+  it("ceil(4001, -2), should return 4100", () => {
+    expect(ceil(4001, -2)).to.equal(4100);
+  });
+  it("ceil(-2.1), should return -2", () => {
+    expect(ceil(-2.1)).to.equal(-2);
+  });
+  it("ceil(-2.345, 2), should return -2.34", () => {
+    expect(ceil(-2.345, 2)).to.equal(-2.34);
+  });
+  it("ceil(4.9, 0), should return 5", () => {
+    expect(ceil(4.9, 0)).to.equal(5);
+  });
+  it("ceil(5), should return 5", () => {
+    expect(ceil(5)).to.equal(5);
+  });
+  it("ceil(0), should return 0", () => {
+    expect(ceil(0)).to.equal(0);
+  });
+  it("ceil(NaN), should return NaN", () => {
+    expect(Number.isNaN(ceil(NaN))).to.be.true;
+  });
+
+});
+
